ecb20e2d60c29967b4f3859f68057ae140ecf607
Cas9Diego <DaCastro@outlook.com>
2022-08-06 15:43:16 -0500

    The program gets the names of the files in the assets catalog and put is into an array, it also filters and trims the names of the files, to get only the names of the images we need

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    var countries = [String]()
+    var items = [String]()
+    var filteredTrimmedItems = [String]()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let strPath = "/Users/diegocastro/Desktop/100 day of swift/GuessTheFlag/GuessTheFlag/Assets.xcassets"
+        let directoryContents = try? fm.contentsOfDirectory(atPath: strPath)
+        items = directoryContents ?? []
+        let filteredItems  = items.filter { $0 != "Contents.json"  && $0 != "AccentColor.colorset" && $0 != "AppIcon.appiconset"}
+        for i in 0..<filteredItems.count {
+            filteredTrimmedItems.append(filteredItems[i].replacingOccurrences(of: ".imageset", with: ""))
+        }
+        
+        print (filteredTrimmedItems)
+        
+        
     }
 
 
